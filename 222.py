3be13f0d5cf8ea41ef25861860d16fc3a3e9b0d6
lishuaiae <lishuaiae@digitalchina.com>
2026-08-05 18:36:30 +0800

    测试

---
+++ 222.py
@@ -0,0 +1,37 @@
+{
+  "version": "1.7.4",
+  "cid": "7204554a-5b3a-4938-8d30-b627cb5c1f82",
+  "data": {
+    "canvas": "28fc059c51e4f1f1cc71a09bb4ebc900",
+    "webgl": "ebad3fd5b15cc3f80ee2bb71cc9be796",
+    "ips": [],
+    "devs": [],
+    "epl": 5,
+    "ep": "8405646e6ff161c831d2fc83d02ade96",
+    "epls": "P5ca3db898597f5b8f9538650160a1f4f,Cb20cf058327d587be3f016cfd357c26a,Me2d1c8a5868ccc5544ef60411f51ab25,W7e54a35bc5090e1b0bf29d19ad33bd99",
+    "fonts": "6710ddc2c90565a861484a8126f8a441",
+    "erd": ",,",
+    "nacn": "Mozilla",
+    "nan": "Netscape",
+    "nce": true,
+    "nlg": "zh-CN",
+    "npf": "Win32",
+    "scd": 24,
+    "etz": "Asia/Shanghai",
+    "etzo": -480,
+    "ndm": 8,
+    "ua": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36",
+    "nhc": 8,
+    "nwd": false,
+    "math": "08b5a8c4bf32e03dd179693fc1e8c15b",
+    "nts": "7ea6734ad56ecce40f9c08ae890290a2",
+    "nmtp": 10
+  },
+  "meta": {
+    "timeStart": 1785925660213,
+    "time": 40,
+    "domFp": "ready",
+    "domFpStart": 1785925660308,
+    "domFpTime": 51
+  }
+}
